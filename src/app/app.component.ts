7379ec964bc80f815372f4c5ad38d8cd7f72d79a
Joachim Butz <Joachim.Butz@wienit.at>
2020-03-23 18:54:29 +0100

    try to fix chartModule is not a function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Chart} from 'angular-highcharts';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html'
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
   constructor() { }
 
   chartExists = false;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
-import { AppComponent } from './app.component';
-
+import { NgModule } from '@angular/core';
+import {ChartModule, HIGHCHARTS_MODULES} from 'angular-highcharts';
 import * as exporting from 'highcharts/modules/exporting.src';
 
-export function highchartsModules() {
-  // apply Highcharts Modules to this array
-  // return [];
-  return [exporting];
-}
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ChartModule ],
-  declarations: [ AppComponent ],
-    providers: [
-    { provide: HIGHCHARTS_MODULES, useFactory: highchartsModules } // add as factory to your providers
+  declarations: [
+    AppComponent
   ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule, ChartModule
+  ],
+  providers: [
+    { provide: HIGHCHARTS_MODULES, useFactory: () => [exporting]}
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
